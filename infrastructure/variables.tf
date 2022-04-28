8f96a33173092d6ade5d1bf1416176cf16bd3fa8
SmithaAJ <smitha.rama@gmail.com>
2022-04-28 21:10:07 +1000

    update to variable

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -8,3 +8,19 @@ variable "location" {
   description = "location where to create vm"
   default = "Australia Southeast"
 }
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+}
+
+variable "client_secret" {
+  description = "Azure client secret"
+}
+
+variable "client_id" {
+  description = "Azure client ID"
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+}
